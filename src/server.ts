5d3999354ec26e82fa41b621a37498ddb692ff52
Mateus Itikawa <mateusairj@gmail.com>
2023-11-24 00:19:07 -0300

    feat: create home banzuke and compare pages

--- src/server.ts
+++ src/server.ts
@@ -29,6 +29,15 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/../public', 'index.html'));
 });
 
+app.get('/banzuke', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../public', 'banzuke.html'));
+});
+
+app.get('/compare', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../public', 'compare.html'));
+});
+
+
 const getWrestlerInfoFromSumoAssociation = async (wrestlerId: string) => {
     const wrestlerProfileUrl = `https://www.sumo.or.jp/EnSumoDataRikishi/profile/${wrestlerId}/`;
     console.log(`Retrieving ${wrestlerId} info from sumo.or.jp.`)
